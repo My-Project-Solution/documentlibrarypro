3c65036e09b4d7a476752f1df15bee2d07a58234
Chris Bradley <chris@myprojectsolution.net>
2021-08-14 09:41:50 -0400

    Update dlp-category-slug.php

--- dlp-category-slug.php
+++ dlp-category-slug.php
@@ -1,1 +1,4 @@
-
+<?php 
+/* Shortcode to display the document library for a specific post based on the Slug. The Post slug and Category Slug must match */
+  $slug = get_post( $optional_id )->post_name;
+	echo do_shortcode('[doc_library doc_category="'.$slug.'"]');
